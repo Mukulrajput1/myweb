84d953c8b2cc5d2093b70c45b0d0f26545299f3c
Mukul Rajput <mr56323885@gmail.com>
2024-03-08 00:36:44 +0530

    seventeenth commit

--- mongoConn.js
+++ mongoConn.js
@@ -1,13 +1,14 @@
 
 const mongoose = require("mongoose");
+require('dotenv').config()
 async function mainConnect() {
   mongoose
     // .connect("mongodb://127.0.0.1:27017/myweb")
-    .connect("mongodb+srv://mukul:5632@cluster0.dqrtgxm.mongodb.net/myweb")
+    .connect(process.env.MONGO_URI)
     .then((res) => {
       console.log("mongo connection is done");
     })
-    .catch(() => {
+    .catch((res) => {
       console.log("failed to connect with mongo");
       res.send(200)
     });
